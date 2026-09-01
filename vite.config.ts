040f37d8866e596cf77c9bf93c4b3d97bacb19b2
samirghurbanov <118278092+samirghurbanov@users.noreply.github.com>
2026-09-01 13:00:19 +0400

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/yasemen-samir-engagement/',
+
   server: {
     port: 3000,
     host: true,
-    base: '/yasemen-samir-engagement/'
-  }
-});
+  },
+})
